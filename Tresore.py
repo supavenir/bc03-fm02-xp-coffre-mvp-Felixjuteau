e3b37b900dd58a217b57a9e55e850d71c0acc8bd
Lubin ALIX-VOISINE <114068754+ALIXLUBIN@users.noreply.github.com>
2026-01-07 09:44:44 +0100

    object  et suppresion pycache

---
+++ Tresore.py
@@ -0,0 +1,8 @@
+class Tresore:
+    def __init__(self, name, weight, value):
+        self.name = name
+        self.weight = weight
+        self.value = value
+        
+    def getWeight(self):
+        return self.weight
